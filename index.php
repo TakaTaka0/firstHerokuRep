2b2153cbd83290a4af76b2495b39fd0641174672
TakaTaka 0 <tk.1987.6.9@gmail.com>
2017-08-06 18:24:25 +0900

    Logic

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php 
 
-echo "testHeroku";
+
+
+for ($i<=0; $i<=10; $i++){
+    
+        $split = 10;
+        if($split/$i === 0) {
+             $getNum[] = $split/$i;
+             echo $getNum;
+             }
+    
+    
+    
+    
+    }
+
+
+
 
 ?>
