57f0d3d3a620b58bc8469a7b23ca029b7effb06b
TakaTaka 0 <tk.1987.6.9@gmail.com>
2017-09-03 12:09:47 +0900

    testPush

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php 
+echo "testAAAA";
 
 
-
-for ($i<=0; $i<=10; $i++){
-    
-        $split = 10;
-        if($split/$i === 0) {
-             $getNum[] = $split/$i;
-             echo $getNum;
-             }
-    
-    
-    
-    
-    }
 
 
 
